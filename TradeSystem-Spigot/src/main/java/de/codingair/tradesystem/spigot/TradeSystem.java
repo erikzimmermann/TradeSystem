24653e6643989f7748a17cec6bfec58e9fd0255e
Erik Zimmermann <mail@erikzimmermann.de>
2023-03-15 10:54:12 +0100

    [TS-427] Fix plugin load order Closes #427

--- TradeSystem-Spigot/src/main/java/de/codingair/tradesystem/spigot/TradeSystem.java
+++ TradeSystem-Spigot/src/main/java/de/codingair/tradesystem/spigot/TradeSystem.java
@@ -35,8 +35,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
-import org.bukkit.plugin.InvalidDescriptionException;
-import org.bukkit.plugin.InvalidPluginException;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
