af4654a135622d6c5d9b7783673431a313662855
Erik Zimmermann <mail@erikzimmermann.de>
2024-10-20 12:16:49 +0200

    Make throwable exception in SqlLiteConnection more specific

--- TradeSystem-Spigot/src/main/java/de/codingair/tradesystem/spigot/database/migrations/sqlite/SqlLiteConnection.java
+++ TradeSystem-Spigot/src/main/java/de/codingair/tradesystem/spigot/database/migrations/sqlite/SqlLiteConnection.java
@@ -5,11 +5,12 @@ import org.jetbrains.annotations.NotNull;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SqlLiteConnection {
 
     @NotNull
-    public static Connection connect() throws Exception {
+    public static Connection connect() throws SQLException {
         //get correct case-sensitive plugins-folder name
         String name = TradeSystem.getInstance().getDataFolder().getParentFile().getName();
         String url = "jdbc:sqlite:" + name + "/TradeSystem/tradelog.db";
